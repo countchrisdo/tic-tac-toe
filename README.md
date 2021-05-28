51a37607be0261e6ed143de0ce8d7a7e86cff43d
countchrisdo <crburwell@yahoo.com>
2021-05-28 19:00:13 -0400

    Starting Project Creating Skeleton

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# tic-tac
+Welcome to Tic Tac Toe
+
+If you don't know how to play this game... yikes...
+
+Grab a friend and click the squares
---
+++ js/main.js
@@ -0,0 +1,82 @@
+console.log("Code running");
+
+/*----- constants -----*/
+
+
+
+/*----- app's state (variables) -----*/
+
+let gameStatus; // null, "win1", "win2"
+let testVarStr = "test!";
+let testVarNum = 10;
+
+/*----- cached element references -----*/
+
+
+const buttonEl = document.getElementById("replay");
+const msgEl = document.getElementById("msg");
+
+/*----- event listeners -----*/
+
+document.querySelector("section").addEventListener("click", handleSquareClick);
+document.getElementById("replay").addEventListener("click", init);
+
+/*----- functions -----*/
+
+init();
+
+function init() {
+    //rng
+    const randomIdx = Math.floor(Math.random() * testVarStr.length);
+    
+    gameStatus = null;
+   
+    render();
+}
+
+function render() {
+    
+    renderButtons();
+    renderMessage();
+
+    console.log("Render has run / Page Updated");
+}
+
+function renderButtons() {
+    
+}
+
+function renderMessage() {
+    //win message
+    if (gameStatus === "win1") {
+        msgEl.innerText = "Player 1 win"
+    } else if (gameStatus === "win2") {
+        msgEl.innerText = "Player 2 win"
+    } else { 
+    //state player's turn
+    msgEl.innerHTML = `Player ${testVar} turn! <br> Good Luck!`
+    }
+}
+
+function handleSquareClick(evt){
+    //if not a button, do nothing
+    if (evt.target.tagName !== "BUTTON") return;
+    //if not in game, do nothing??
+    if (gameStatus) return;
+
+    const testVar = evt.target.textContent;
+    console.log(testVar);
+    
+    gameStatus = getGameStatus();
+    console.log(gameStatus);
+    render();
+}
+
+// null, "win1, "win2"
+function getGameStatus() {
+    if (testVar === testVar) return "win1";
+    if (testVar === testVar) return "win2";
+    return null;
+}
+
+console.log("End of the line: Code Ran");
